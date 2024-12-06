b9498b4d5822f9073e7a878460b3db918a90cfde
MJHG01 <garciamarkjonel45@gmail.com>
2024-12-07 02:19:37 +0800

    Game Settings

---
+++ Settings.py
@@ -0,0 +1,12 @@
+import pygame as pg
+import os
+"""This module keeps settings of Game."""
+WIDTH=600
+HEIGHT=700
+FPS=30
+TEXTSIZE=30 
+TEXTCOLOR=(255,255,255)
+
+PGIMLOAD=pg.image.load 
+OSPJ=os.path.join
+path='spaceArt'+os.sep
